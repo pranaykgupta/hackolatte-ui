f172a13704dcf0319351c015553b30b3c1409c92
pranaykgupta <pranaykgupta21@gmail.com>
2020-02-08 18:32:38 +0530

    linked hospi login

--- hospital/call.js
+++ hospital/call.js
@@ -4,33 +4,55 @@
 
 //     })
 // );
+function give_msg(message){
+  $('#msg').html(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>${message}</strong>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  </div>`);
+  return;
+}
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 
 $('#login_sub').on('click',function(event){
+    if($('#email_id').val() == ''){
+      give_msg('Please enter your email address.');
+    }else if($('#pass').val() == ''){
+      give_msg('Please enter your password.');
+    }else if(validateEmail($('#email_id').val()) == false){
+      give_msg("Enter valid email id!");
+    }
+    else{
     let email = $('#email_id').val();
     let password = $('#pass').val();
     $.ajax({
         type: "POST",
-        url: "login_route",
+        url: "login_route.php",
         data: {
             email: email,
             password: password
         },
         success: function(data){
             data = JSON.parse(data);
-            if(data['status']==200){
+            if(data['status']==202){
                 // setting session variables
+                sessionStorage.message = data['message'];
+                sessionStorage.access_token = data['access_token'];
                 // and redirecting to work page
+                window.location.href = 'show.html';
 
-            }else if(data['status'] == 400){
-                $('#msg').html(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <strong>${data['message']}</strong> You should check in on some of those fields below.
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>`);
+            }else if(data['status'] == 401 || data['status'] == 422){
+              give_msg(data['message']);
             }
         }
     });
+    }
 });
 
 $('#add_new').on('click',function(event){
---
+++ hospital/login_route.php
@@ -0,0 +1,7 @@
+<?php
+ $response = array();
+ $response['status'] = 202;
+ $response['message'] = "successful";
+ $response['access_token'] = 'jfldskjfliw343j5kj6l';
+echo json_encode($response);
+?>
